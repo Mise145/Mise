17648e90e046f2df1e1b13d92e855a3e4a26174c
Mise145 <127885683+Mise145@users.noreply.github.com>
2023-03-15 13:25:17 +0500

    Create Auto Exchange.lua

---
+++ Auto Exchange.lua
@@ -0,0 +1,79 @@
+script_name("Auto Exchange")
+script_version("1.00")
+
+local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
+local autoupdate_loaded = false
+local Update = nil
+if enable_autoupdate then
+    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
+    if updater_loaded then
+        autoupdate_loaded, Update = pcall(Updater)
+        if autoupdate_loaded then
+            Update.json_url = "https://raw.githubusercontent.com/qrlk/moonloader-script-updater/master/minified-example.json?" .. tostring(os.clock())
+            Update.prefix = "[" .. string.upper(thisScript().name) .. "]: "
+            Update.url = "https://github.com/qrlk/moonloader-script-updater/"
+        end
+    end
+end
+local tag = "[Auto exchange]: "
+local sampev = require("samp.events")
+local vkeys = require "vkeys"
+local state = true
+require "lib.sampfuncs"
+function main()
+	if not isSampLoaded() or not isSampfuncsLoaded() then return end
+	while not isSampAvailable() do wait(100) end
+
+	if autoupdate_loaded and enable_autoupdate and Update then
+        pcall(Update.check, Update.json_url, Update.prefix, Update.url)
+    end
+
+    _, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
+    name = sampGetPlayerNickname(id)
+    if name ~= "Miset_Basotskiy" then
+        thisScript():unload()
+    end
+
+	sampRegisterChatCommand("Auto", cmd_auto)
+
+	while true do
+		wait(0)
+
+		if state == false then
+			if not sampIsChatInputActive() and not isSampfuncsConsoleActive() and not sampIsDialogActive() then
+				setVirtualKeyDown(18, true)
+				wait(100)
+				setVirtualKeyDown(18, false)
+			end
+		end
+	end
+end
+
+function cmd_auto(arg)
+	state = not state
+	sampAddChatMessage(tag.. (state and 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.' or 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.'), 0xff0000)
+end
+
+function sampev.onShowDialog(id, style, title, button1, button2, tekst)
+	if state == false then
+		if tekst:find('пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ') then
+			lua_thread.create(function()
+			wait(0)
+			listbox = sampGetListboxItemByText('пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ')
+			sampSendDialogResponse(id, 1, listbox, nil)
+			sampCloseCurrentDialogWithButton(0)
+			end)
+		end
+	end
+end
+
+function sampGetListboxItemByText(text, plain)
+    if not sampIsDialogActive() then return -1 end
+        plain = not (plain == false)
+    for i = 0, sampGetListboxItemsCount() - 1 do
+        if sampGetListboxItemText(i):find(text, 1, plain) then
+            return i
+        end
+    end
+    return -1
+end
